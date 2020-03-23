d0139968131a65da27c8bad2725d2ab64fd0acb0
Benjamin Heller <ben@benjaminheller.net>
2020-03-23 13:50:05 -0800

    Add stripped down embed route. Will clean up scss and DRY up later

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="primary" dark v-if="'embed' !== this.$route.name">
       <h1>Alaska Coronavirus</h1>
     </v-app-bar>
-
     <v-content>
       <router-view />
     </v-content>
---
+++ src/views/Embed.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="quick-data">
+    <h3>Alaska Coronavirus Quick Data</h3>
+    <p>Total Cases {{ alaska.cases }}</p>
+    <p>Active Cases {{ alaska.active }}</p>
+    <p>Recovered {{ alaska.recovered }}</p>
+    <p>New Cases today {{ alaska.todayCases }}</p>
+    <p>Total Deaths {{ alaska.deaths }}</p>
+    <p>Today's Deaths {{ alaska.todayDeaths }}</p>
+  </div>
+</template>
+
+<script>
+const novelcovid = require('novelcovid')
+
+export default {
+  name: 'Embed',
+  data() {
+    return {
+      states: '',
+      alaska: {},
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      this.states = await novelcovid.getState()
+      this.processData()
+    },
+
+    processData() {
+      for (let i = 0; i < this.states.length; i++) {
+        if ('Alaska' === this.states[i].state) {
+          console.log(this.states[i].state)
+          this.alaska = this.states[i]
+        }
+      }
+    },
+  },
+}
+</script>
+<style lang="scss">
+h3 {
+  margin-bottom: 1em;
+}
+
+p {
+  font-size: 20px;
+  line-height: 1em;
+  margin: 0;
+  padding: 0;
+}
+</style>
